7254a534ad85228418aece9810bc00c44c71d97e
Connie Yao <yao.connie1@gmail.com>
2020-02-13 13:02:02 -0500

    proxy

---
+++ cs2212-project-documented/src/ca/uwo/proxies/LowQuantityProxy.java
@@ -0,0 +1,23 @@
+package ca.uwo.proxies;
+
+import java.util.Map;
+
+import ca.uwo.client.Buyer;
+import ca.uwo.client.Supplier;
+import ca.uwo.frontend.Facade;
+
+public class LowQuantityProxy extends Proxy{
+
+	@Override
+	public void placeOrder(Map<String, Integer> orderDetails, Buyer buyer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void restock(Map<String, Integer> restockDetails, Supplier supplier) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ cs2212-project-documented/src/ca/uwo/proxies/SupplierProxy.java
@@ -0,0 +1,25 @@
+package ca.uwo.proxies;
+
+import java.util.Map;
+
+import ca.uwo.client.Buyer;
+import ca.uwo.client.Supplier;
+import ca.uwo.frontend.Facade;
+
+public class SupplierProxy extends Proxy{
+	public SupplierProxy() {
+		
+	}
+
+	@Override
+	public void placeOrder(Map<String, Integer> orderDetails, Buyer buyer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void restock(Map<String, Integer> restockDetails, Supplier supplier) {
+		// TODO Auto-generated method stub
+		
+	}
+}
